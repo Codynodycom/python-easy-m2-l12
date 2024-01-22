028cbc9a448ce4396ab4e0e0a4039cb5e92800b8
youjintyan <tyanbz@gmail.com>
2024-01-22 22:45:31 +0600

    created

--- tasks. part 2/task 2/m2l12t2.py
+++ tasks. part 2/task 2/m2l12t2.py
@@ -1,9 +1,14 @@
 '''
 Задача 2
 
+Исправьте ошибки в коде.
 
+Исправьте ошибки, чтобы код печатал каждое слово и его длину.
 
 '''
 
+words = ["apple", "banana", "cherry"]
+for word in words
+    print(word + len(word))
 
 
--- tasks. part 2/task 3/m2l12t3.py
+++ tasks. part 2/task 3/m2l12t3.py
@@ -1,9 +1,15 @@
 '''
 Задача 3
 
+Допишите код.
 
+Допишите код, чтобы он заполнял список reversed_numbers числами из списка numbers в обратном порядке.
 
 '''
 
+numbers = [10, 20, 30, 40, 50]
+reversed_numbers = []
+# Ваш код здесь
+print(reversed_numbers)
 
 
